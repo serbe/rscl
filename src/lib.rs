47691713aff526bd2a82b4dde644c84d9f0cf6a8
Serbe <serbenv@gmail.com>
2023-05-23 11:42:04 +0300

    fix tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,39 +2,5 @@ pub mod consts;
 pub mod error;
 pub mod socks4;
 pub mod socks5;
-pub mod utils;
 
 pub use error::Error;
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use tokio::{
-//         io::{AsyncReadExt, AsyncWriteExt},
-//         net::TcpStream,
-//     };
-
-// #[tokio::test]
-// async fn conn_v5() {
-//     let mut stream = socks5::connect("socks5://127.0.0.1:5959", "http://api.ipify.org")
-//         .await
-//         .unwrap();
-//     stream.write_all(b"GET / HTTP/1.0\r\n\r\n").await.unwrap();
-//     stream.flush().await.unwrap();
-//     let mut buf = Vec::new();
-//     stream.read_to_end(&mut buf).await.unwrap();
-//     assert!(!buf.is_empty());
-// }
-
-// #[tokio::test]
-// async fn conn_v4() {
-//     let mut stream = socks4::connect("socks4://127.0.0.1:5959", "http://api.ipify.org")
-//         .await
-//         .unwrap();
-//     stream.write_all(b"GET / HTTP/1.0\r\n\r\n").await.unwrap();
-//     stream.flush().await.unwrap();
-//     let mut buf = Vec::new();
-//     stream.read_to_end(&mut buf).await.unwrap();
-//     assert!(!buf.is_empty());
-// }
-// }
--- tests/test_socks5.rs
+++ tests/test_socks5.rs
@@ -1,84 +1,73 @@
 use log::debug;
 use rscl::socks5::SocksClient;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+};
 
-const SIMPLE_URL: &'static str = "http://api.ipify.org";
+const SIMPLE_URL: &'static str = "http://httpbin.smp.io/ip";
 
 fn init_logger() {
     dotenv::dotenv().ok();
     let _ = env_logger::builder().try_init();
 }
 
+async fn get_client(env_var: &str) -> SocksClient<TcpStream> {
+    let proxy = dotenv::var(env_var).unwrap();
+
+    SocksClient::new(proxy.parse().unwrap(), SIMPLE_URL.parse().unwrap())
+        .await
+        .unwrap()
+}
+
+async fn get_body(client: &mut SocksClient<TcpStream>) -> String {
+    client.handshake().await.unwrap();
+    client
+        .write_all(b"GET /ip HTTP/1.0\r\nHost: httpbin.smp.io\r\n\r\n")
+        .await
+        .unwrap();
+    client.flush().await.unwrap();
+    let mut buf = Vec::new();
+    client.read_to_end(&mut buf).await.unwrap();
+    String::from_utf8(buf).unwrap()
+}
+// TEST_SOCKS5_PROXY - an environment variable containing the socks5 server address without authorization. For example:
+// socks5://127.0.0.1:3128
 #[tokio::test]
 async fn test_socks_client() {
     init_logger();
 
-    let test_var = "TEST_SOCKS5_PROXY";
+    let env_var = "TEST_SOCKS5_PROXY";
 
-    let proxy = match dotenv::var(test_var) {
-        Ok(it) => it,
-        _ => return,
-    };
+    let mut client = get_client(env_var).await;
+    let body = get_body(&mut client).await;
 
-    let mut client = SocksClient::new(proxy.parse().unwrap(), SIMPLE_URL.parse().unwrap())
-        .await
-        .unwrap();
-
-    client.handshake().await.unwrap();
-
-    client
-        .write_all(b"GET / HTTP/1.0\r\nHost: api.ipify.org\r\n\r\n")
-        .await
-        .unwrap();
-    client.flush().await.unwrap();
-    let mut buf = Vec::new();
-    client.read_to_end(&mut buf).await.unwrap();
-    let body = String::from_utf8(buf).unwrap();
     debug!("body {}", body);
 }
 
+// TEST_SOCKS5_AUTH_PROXY - an environment variable containing the socks5 server address with authorization. For example:
+// socks5://username:password@127.0.0.1:3128
 #[tokio::test]
 async fn test_auth_socks_client() {
     init_logger();
 
-    let test_var = "TEST_AUTH_PROXY";
+    let env_var = "TEST_SOCKS5_AUTH_PROXY";
 
-    let proxy = match dotenv::var(test_var) {
-        Ok(it) => it,
-        _ => return,
-    };
+    let mut client = get_client(env_var).await;
+    let body = get_body(&mut client).await;
 
-    let mut client = SocksClient::new(proxy.parse().unwrap(), SIMPLE_URL.parse().unwrap())
-        .await
-        .unwrap();
-
-    client.handshake().await.unwrap();
-
-    client
-        .write_all(b"GET / HTTP/1.0\r\nHost: api.ipify.org\r\n\r\n")
-        .await
-        .unwrap();
-    client.flush().await.unwrap();
-    let mut buf = Vec::new();
-    client.read_to_end(&mut buf).await.unwrap();
-    let body = String::from_utf8(buf).unwrap();
     debug!("body {}", body);
 }
 
+// TEST_SOCKS5_AUTH_PROXY - an environment variable containing the address of the socks5 server with erroneous authorization. For example:
+// socks5://wrong_username:wrong_password@127.0.0.1:3128
 #[tokio::test]
 async fn test_auth_socks_err_client() {
     init_logger();
 
-    let test_var = "TEST_ERR_AUTH_PROXY";
+    let env_var = "TEST_SOCKS5_AUTH_ERR_PROXY";
 
-    let proxy = match dotenv::var(test_var) {
-        Ok(it) => it,
-        _ => return,
-    };
-
-    let mut client = SocksClient::new(proxy.parse().unwrap(), SIMPLE_URL.parse().unwrap())
-        .await
-        .unwrap();
+    let mut client = get_client(env_var).await;
 
     assert!(client.handshake().await.is_err());
 }
